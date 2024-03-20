b41a79bd7283b44a414040776a748172932e2059
Juan5 <juangm2302@gmail.com>
2024-03-20 15:04:58 -0600

    Extensions Of Components from home has been changed and set the Navbar in Layout

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,10 @@
-import { Manrope } from "next/font/google";
-import NavBar from "../components/Home/NavBar/NavBar";
 import ShipSearchBar from "@/components/Home/ShipSearchBar/shipSearchBar";
 import Filter from "@/components/Home/Filter/Filter";
 import CardContainer from "@/components/Home/CardContainer/CardContainer";
 
-const manrope = Manrope({ subsets: ["latin"], variable: "--font-manrope" });
-
 const HomePage = () => {
   return (
-    <main className={`${manrope.variable} font-mono`}>
-      <NavBar />
+    <main>
       <ShipSearchBar />
       <div className="mx-8">
         <hr className="my-4 border-gray-300 " />
